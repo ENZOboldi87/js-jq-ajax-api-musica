7345f3dc5969ea3b4f3b3166e1cfe52398b189f5
Giuseppe Rotolo <rotolo37@gmail.com>
2020-06-26 15:20:53 +0200

    aggiornamento

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-	//Code
+
+  $.ajax({
+    url: 'https://flynn.boolean.careers/exercises/api/array/music',
+    type: 'GET',
+    success: function (dischi) {
+      var cds = dischi.response;
+      showCd(cds);
+    },
+    error:(function() {
+      alert('errore');
+    })
+  })
+
+// funzione che stampa i cd nel container
+function showCd(arrayCd) {
+  var source = $("#cd-template").html();
+  var template = Handlebars.compile(source);
+
+
+  for (var i = 0; i < arrayCd.length; i++) {
+    var singoloCd = arrayCd[i];
+    var html = template(singoloCd);
+    $('.cds-container').append(html);
+  }
+
+
+}
+
+
+
+
 });
