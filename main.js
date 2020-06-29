91dda16958776a0f9cd19ab69a25a867d008e00c
Giuseppe Rotolo <rotolo37@gmail.com>
2020-06-29 10:02:13 +0200

    bug fixed

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
 $(document).ready(function() {
 
+// chiamata ajax per generare i rispettivi album
   $.ajax({
     url: 'https://flynn.boolean.careers/exercises/api/array/music',
     type: 'GET',
+    data: {'genre': 'Rock'},
     success: function (dischi) {
       var cds = dischi.response;
       showCd(cds);
     },
+
     error:(function() {
       alert('errore');
     })
+
   })
+
+  $(document).on('change', 'header select', function(){
+    var opzione = $(this).val();
+    if (opzione === "All") {
+      $('.cd').show();
+    }
+    else {
+      $('.cd').hide();
+      $('.cd.' + opzione).show();
+    }
+    })
+
+
+
 
 // funzione che stampa i cd nel container
 function showCd(arrayCd) {
   var source = $("#cd-template").html();
   var template = Handlebars.compile(source);
-
 
   for (var i = 0; i < arrayCd.length; i++) {
     var singoloCd = arrayCd[i];
@@ -26,8 +43,6 @@ function showCd(arrayCd) {
 
 
 }
-
-
 
 
 });
